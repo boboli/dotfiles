701d9556631e4dfb4cc3cf0e732938542a838e2a
Bobo Li <bobo@robinhood.com>
2016-02-28 21:20:08 -0800

    Fixed execution of YCM installer

--- init/50_vim.sh
+++ init/50_vim.sh
@@ -4,5 +4,5 @@ mkdir -p $DOTFILES/caches/vim
 # Download Vim plugins.
 if [[ "$(type -P vim)" ]]; then
   vim +PluginInstall +qall
-  cd ~/.vim/bundle/YouCompleteMe && install.py --clang-completer
+  cd ~/.vim/bundle/YouCompleteMe && ./install.py --clang-completer
 fi
