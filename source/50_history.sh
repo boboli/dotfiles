b9e468e83ea620c51ec5b33a99ca00293f36491d
Bobo Li <bobo@robinhood.com>
2016-02-28 23:57:29 -0800

    Converted some more bash-isms into zsh-isms

--- source/50_history.sh
+++ source/50_history.sh
@@ -1,9 +1,9 @@
 # History settings
 
 # Allow use to re-edit a faild history substitution.
-shopt -s histreedit
+setopt -s histreedit
 # History expansions will be verified before execution.
-shopt -s histverify
+setopt -s histverify
 
 # Entries beginning with space aren't added into history, and duplicate
 # entries will be erased (leaving the most recent entry).
--- source/50_misc.sh
+++ source/50_misc.sh
@@ -1,9 +1,9 @@
 # Case-insensitive globbing (used in pathname expansion)
-shopt -s nocaseglob
+setopt -s nocaseglob
 
 # Check the window size after each command and, if necessary,
 # update the values of LINES and COLUMNS.
-shopt -s checkwinsize
+setopt -s checkwinsize
 
 export GREP_OPTIONS='--color=auto'
 
