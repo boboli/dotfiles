207613555d7521463535bc83c94d97194dc13491
Bobo Li <bobo@robinhood.com>
2016-02-29 17:17:07 -0800

    Also update vim plugins on future runs of dotfiles

--- init/50_vim.sh
+++ init/50_vim.sh
@@ -3,6 +3,6 @@ mkdir -p $DOTFILES/caches/vim
 
 # Download Vim plugins.
 if [[ "$(command -v vim)" ]]; then
-  vim +PluginInstall +qall
+  vim +PluginInstall +PluginUpdate +qall
   cd ~/.vim/bundle/YouCompleteMe && ./install.py --clang-completer
 fi
