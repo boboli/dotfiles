9ca7b786442098c2bd1888321aede2ee84b7b973
Bobo Li <bobo@robinhood.com>
2016-03-05 17:43:24 -0800

    Fixed terraform PATH to the correct location

--- source/50_path.sh
+++ source/50_path.sh
@@ -1,4 +1,4 @@
 export PATH=$PATH:~/bin
 export PATH=$PATH:/opt/local/bin:/opt/local/sbin:/opt/local/include
 export PATH=$PATH:~/compiles/arcanist/bin
-export PATH=$PATH:~/bin/terraform
+export PATH=$PATH:~/compiles/terraform
